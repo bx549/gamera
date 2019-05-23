5b86f6bdd10ff32a79d1b4174d529eb1acb3f13f
bx549 <bx549@protonmail.com>
2019-05-23 17:58:43 -0500

    change model to linear regression model using lm() response variable is delta.bid

--- src/training.R
+++ src/training.R
@@ -1,18 +1,15 @@
 datadir <- "~/Downloads/data/train/"
+exchange <- "kraken"   # "bittrex" or "kraken"
+granularity <- 10
 source("prep-data.R")
 
 ## fit a logistic regression model
-fm.bid.up <- glm(delta.bid > 0 ~
-                     net.order.flow +
-                     I(lag(delta.buy.dist)/lag(ts.diff)),
-                 data=Obook, family=binomial(link="logit"))
-summary(fm.bid.up)
+## pay attention to the time step! the response variable is lead(.)
+fm.bid <- lm(lead(delta.bid) ~ delta.bid + net.order.flow + delta.buy.dist, data=Obook)
+summary(fm.bid)
 
 # diagnostics for the regression model
 layout(matrix(1:4, nrow=2))
-plot(fm.bid.up)
+plot(fm.bid)
 
-
-
-
-
+save(fm.bid, file="fm.rda")  # save the fitted model
